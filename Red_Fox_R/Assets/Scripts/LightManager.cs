8e5efcd3949cae4618f8aed228efa9cc6acd43df
David Dunnings <David2.Dunnings@live.uwe.ac.uk>
2015-06-26 23:38:40 +0100

    New Project "3D"

---
+++ Red_Fox_R/Assets/Scripts/LightManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LightManager : MonoBehaviour
+{
+
+    public GameObject player;
+    public List<GameObject> allLights;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool isInAnyLight(GameObject player, List<GameObject> allLights)
+    {
+        bool isInAny = false;
+        for (int i = 0; i < allLights.Count; i++)
+        {
+            if (isInLight(player, allLights[i]))
+            {
+                isInAny = true;
+            }
+        }
+        return isInAny;
+    }
+
+    private bool isInLight(GameObject player, GameObject light)
+    {
+        RaycastHit2D newHit = Physics2D.Raycast(new Vector2(player.transform.position.x, player.transform.position.y), new Vector2(light.transform.position.x, light.transform.position.y));
+        return (newHit.collider != null);
+    }
+}
