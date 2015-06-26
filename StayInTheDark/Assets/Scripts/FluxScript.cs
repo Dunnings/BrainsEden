80985e1d59d3f1c82db63565dd3c34e509f38d3d
David Dunnings <David2.Dunnings@live.uwe.ac.uk>
2015-06-26 21:17:44 +0100

    Main Menu Done

--- StayInTheDark/Assets/Scripts/FluxScript.cs
+++ StayInTheDark/Assets/Scripts/FluxScript.cs
@@ -1,16 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class FluxScript : MonoBehaviour {
 
+    private float startXScale;
+    private float endXScale;
+    private bool expand = true;
+
+    public RectTransform r;
 
 	// Use this for initialization
-	void Start () {
-	
+    void Start()
+    {
+        startXScale = r.rect.xMax;
+        endXScale = r.rect.xMax + 20f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.localScale = new Vector3(Time.deltaTime * 100.0f, transform.localScale.y, transform.localScale.z);
+        if (expand)
+        {
+            r.sizeDelta.Scale(new Vector2(r.rect.xMax + 0.5f * Time.deltaTime, 1f));
+            if (r.rect.xMax >= endXScale)
+            {
+                expand = false;
+            }
+        }
+        else
+        {
+            r.sizeDelta.Scale(new Vector2(r.rect.xMax - 0.5f * Time.deltaTime, 1f));
+            if (r.rect.xMax <= startXScale)
+            {
+                expand = true;
+            }
+        }
 	}
 }
