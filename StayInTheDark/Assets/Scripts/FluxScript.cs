6f440c4df0affea27af2451512dde4e3cf2a32f0
David Dunnings <David2.Dunnings@live.uwe.ac.uk>
2015-06-26 20:34:53 +0100

    Working on menu

---
+++ StayInTheDark/Assets/Scripts/FluxScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FluxScript : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.localScale = new Vector3(Time.deltaTime * 100.0f, transform.localScale.y, transform.localScale.z);
+	}
+}
