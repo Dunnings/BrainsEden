59f6a04e64e0acf528b9ebff070f07a561ea6c78
EvilToaster626 <westondaniel93@gmail.com>
2015-06-27 12:00:57 +0100

    level stuff

---
+++ StayInTheDark/Assets/Scripts/oscillation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class oscillation : MonoBehaviour {
+
+    public float factor = 2.0f; 
+
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.position = new Vector3(Mathf.Sin(Time.time) * factor, transform.position.y, transform.position.z);
+	}
+}
