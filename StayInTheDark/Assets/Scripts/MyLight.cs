0de1640d913c7e3498ab8a02c8ed753a69a3d8ea
David Dunnings <David2.Dunnings@live.uwe.ac.uk>
2015-06-27 01:25:27 +0100

    Final New Project All Done

---
+++ StayInTheDark/Assets/Scripts/MyLight.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyLight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(0.2f * Time.deltaTime, 0.0f, 0.0f);
+	}
+}
