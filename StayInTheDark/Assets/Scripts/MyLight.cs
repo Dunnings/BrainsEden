350fcc253e3a87ba6586e9127a6796ca0d96a26f
KieronHusain <kieron2.husain@live.uwe.ac.uk>
2015-06-27 14:52:19 +0100

    Level 1 done

--- StayInTheDark/Assets/Scripts/MyLight.cs
+++ StayInTheDark/Assets/Scripts/MyLight.cs
@@ -5,7 +5,7 @@ public class MyLight : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        transform.position += new Vector3(0.2f * Time.deltaTime, 0.0f, 0.0f);
 	}
 	
 	// Update is called once per frame
--- StayInTheDark/Assets/Scripts/oscillation.cs
+++ StayInTheDark/Assets/Scripts/oscillation.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class oscillation : MonoBehaviour {
 
-    public float factor = 2.0f; 
+    public float factor = 2.0f;
 
+    private Vector3 originalPosition;
+
+    void Start()
+    {
+        originalPosition = transform.position; 
+    }
+    
 	// Update is called once per frame
 	void Update () 
     {
-        transform.position = new Vector3(Mathf.Sin(Time.time) * factor, transform.position.y, transform.position.z);
+        transform.position = new Vector3(originalPosition.x + (Mathf.Sin(Time.time) * factor), transform.position.y, transform.position.z);
+
+        //transform.position = new Vector3(Mathf.Sin(Time.time) * factor, transform.position.y, transform.position.z);
 	}
 }
