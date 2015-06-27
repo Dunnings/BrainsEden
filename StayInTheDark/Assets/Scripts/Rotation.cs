6a4baa4492886d1e0f58cf3efd291803053fe9fa
EvilToaster626 <westondaniel93@gmail.com>
2015-06-27 11:23:07 +0100

    hex prefab

--- StayInTheDark/Assets/Scripts/Rotation.cs
+++ StayInTheDark/Assets/Scripts/Rotation.cs
@@ -3,15 +3,12 @@ using System.Collections;
 
 public class Rotation : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public float speed = 2.0f;
 	
 	// Update is called once per frame
 	void Update ()
     {
-        this.transform.Rotate(new Vector3(0, 0, 1), 0.5f);
+        this.transform.Rotate(new Vector3(0, 0, 1), speed);
 
         //transform.position = new Vector3(Mathf.Sin(Time.time * 0.08f) * 4.0f, transform.position.y, transform.position.z);
     }
