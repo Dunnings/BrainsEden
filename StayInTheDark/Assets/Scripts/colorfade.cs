e8647aad37f1af9fec1b407c8ce706e348d36f36
David Dunnings <David2.Dunnings@live.uwe.ac.uk>
2015-06-27 21:15:11 +0100

    Menu

---
+++ StayInTheDark/Assets/Scripts/colorfade.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colorfade : MonoBehaviour {
+
+    public Camera mainCam;
+    private float startTime;
+    private float duration = 2f;
+
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+        lerpedColor = mainCam.backgroundColor;
+        targetColor = new Color(Random.Range(0.8f, 1f), Random.Range(0.8f, 1f), Random.Range(0.8f, 1f));
+	}
+
+    public Color lerpedColor;
+    public Color targetColor = new Color(0.01f, 0.7f, 0.02f);
+    void Update()
+    {
+        if (Time.time - startTime > duration)
+        {
+            startTime = Time.time;
+            lerpedColor = targetColor;
+            targetColor = new Color(Random.Range(0.8f, 1f), Random.Range(0.8f, 1f), Random.Range(0.8f, 1f));
+        }
+        else
+        {
+            lerpedColor = Color.Lerp(lerpedColor, targetColor, (Time.time - startTime) / duration);
+        }
+        mainCam.backgroundColor = lerpedColor;
+    }
+}
