8bb73a0e7777d0b4cb704a1e98982e4f57ecbb5c
David Dunnings <David2.Dunnings@live.uwe.ac.uk>
2015-06-28 16:30:03 +0100

    DONE

--- StayInTheDark/Assets/Scripts/playerController.cs
+++ StayInTheDark/Assets/Scripts/playerController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class playerController : MonoBehaviour
 {
     public float speed = 10f;
+    public Vector3 lastPos;
     // Update is called once per frame
     void Update()
     {
@@ -15,20 +16,57 @@ public class playerController : MonoBehaviour
 
     void move()
     {
-        if (Input.acceleration.x > 0.08f || Input.acceleration.x < -0.08f || Input.acceleration.y > 0.08f || Input.acceleration.y < -0.08f)
+
+
+        Vector3 dir = (Player.Instance.transform.position - lastPos).normalized;
+        lastPos = Player.Instance.transform.position;
+
+        RaycastHit2D[] daveIloveYou = Physics2D.RaycastAll(new Vector2(Player.Instance.transform.position.x, Player.Instance.transform.position.y), new Vector2(dir.x, dir.y), 1f);
+        Debug.DrawRay((new Vector3(Player.Instance.transform.position.x, Player.Instance.transform.position.y, 0.0f)), new Vector3(dir.x, dir.y, 0.0f));
+
+
+        bool canMove = true;
+        if (daveIloveYou != null)
         {
-            Vector3 translate = new Vector3(Input.acceleration.x, Input.acceleration.y, -Input.acceleration.z);
-            transform.Translate(translate * speed * Time.deltaTime);
+            for (int i = 0; i < daveIloveYou.Length; i++)
+            {
+                if (daveIloveYou[i].collider.gameObject.tag == "wall")
+                {
+                    canMove = false;
+                    break;
+                }
+            }
+        }
+        if (canMove)
+        {
+            if (Input.acceleration.x > 0.08f || Input.acceleration.x < -0.08f
+                || Input.acceleration.y > 0.08f || Input.acceleration.y < -0.08f)
+            {
+                float x = Input.acceleration.x;
+                float y = Input.acceleration.y;
+                float z = Input.acceleration.z;
+                if (x > 0.5f) { x = 0.5f; }
+                if (x < -0.5f) { x = -0.5f; }
+                if (y > 0.5f) { y = 0.5f; }
+                if (y < -0.5f) { y = -0.5f; }
+                
+                Vector3 translate = new Vector3(x,y,0f);
+                transform.Translate(translate * speed * Time.deltaTime);
+            }
+
+
+
+            if (Input.GetButton("Horizontal"))
+            {
+                transform.Translate(new Vector3((Input.GetAxis("Horizontal") * speed * 0.5f * Time.deltaTime), 0.0f, 0.0f));
+            }
+            if (Input.GetButton("Vertical"))
+            {
+                transform.Translate(new Vector3(0.0f, (Input.GetAxis("Vertical") * speed * 0.5f * Time.deltaTime), 0.0f));
+            }
         }
 
-        if (Input.GetButton("Horizontal"))
-        {
-            transform.Translate(new Vector3((Input.GetAxis("Horizontal") * speed * 0.5f * Time.deltaTime), 0.0f, 0.0f));
-        }
-        if (Input.GetButton("Vertical"))
-        {
-            transform.Translate(new Vector3(0.0f, (Input.GetAxis("Vertical") * speed * 0.5f * Time.deltaTime), 0.0f));
-        }
+
 
     }
 }
