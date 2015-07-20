90db3d8be4223d45c41482c88799d33e837d4c5c
Dunnings <mrdaviddunnings@gmail.com>
2015-07-20 19:29:05 +0100

    Added "Shade Ver 2.0" Solution. Integrated stars and movement script. Implemented Start and Finish Points

---
+++ Shade Ver 2.0/Shade/Assets/Star.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	    
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        GameManager.Instance.RewardStar();
+        gameObject.SetActive(false);
+    }
+}
